965e26ed9e1957034dc09c04f399c87eec43aded
zcZhang123 <18856655155@163.com>
2019-01-07 09:45:48 +0800

    change jest test

--- test/unit/specs/KLineMobileSetChart.spec.js
+++ test/unit/specs/KLineMobileSetChart.spec.js
@@ -1,5 +1,7 @@
 import klineMobileSetChart from 'js/KLineMobileSetChart'
 import mobileOption from 'js/KLineMobileOption'
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 describe('test KLineMobileSetChart', () => {
     
@@ -27,6 +29,14 @@ describe('test KLineMobileSetChart', () => {
       width: 780
     }
     expect(mobileKline.getYAxis(size)).not.toBeNull()
+  })
+
+  it('test DOM', () => {
+    let showIndicators = ['Candlestick', 'MA', 'Volume']
+    let div = '<div id="kline" style="height: 750px; width: 1125px; position: relative; background: rgb(22, 27, 33);"><div style="position: relative; width: 1125px; height: 750px;border-width: 0px;"><canvas width="3375" height="2250" style="position: absolute; width: 1125px; height: 750px;"></canvas><canvas width="3375" height="2250" style="position: absolute; width: 1125px; height: 750px;"></canvas></div><div></div></div>'
+    const { document } = (new JSDOM(div)).window;
+    let mobileKline = new klineMobileSetChart(mobileOption, showIndicators);
+    expect(mobileKline.initMobileECharts(document)).toBe()
   })
 
 })
