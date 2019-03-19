2f71812bffb021a728a51a896a9cdc0e1a715dbd
cjzhang1230 <1462588499@qq.com>
2019-03-19 16:20:35 +0800

    MACD字体样式位置调整

--- src/js/MACDOption.js
+++ src/js/MACDOption.js
@@ -1,23 +1,31 @@
 var macdOption = {
     backgroundColor: '#161b21', // K线图背景色，默认'#161b21'
     animation: false, // 是否开启动画。
+    // tooltip: { // 提示框组件。
+    //     trigger: 'axis', //触发类型。当前为坐标轴触发
+    //     animation: false,
+    //     axisPointer: { // 坐标轴指示器配置项。
+    //         type: 'cross', // 指示器类型。当前为十字准星指示器。其实是种简写，表示启用两个正交的轴的 axisPointer。
+    //         link: { xAxisIndex: 'all' }
+    //     },
+    //     backgroundColor: 'rgba(245, 245, 245, 0.5)', // 提示框浮层的背景颜色。
+    //     borderWidth: 1, // 提示框浮层的边框宽。
+    //     borderColor: '#ccc', // 提示框浮层的边框颜色。
+    //     padding: 10, // 提示框浮层内边距，单位px
+    //     textStyle: { // 提示框浮层的文本样式
+    //         // fontSize: '0.1rem', // 文字的字体大小
+    //         color: '#ffffff', // 文字的颜色
+    //         fontFamily: 'Avenir, Helvetica, Arial, sans-serif' // 文字的字体系列
+    //     },
+    //     extraCssText: 'background:#252332;border:0;opacity: 1;fontSize: 18px', // 文字本身的描边颜色
+    // },
     tooltip: { // 提示框组件。
         trigger: 'axis', //触发类型。当前为坐标轴触发
         animation: false,
         axisPointer: { // 坐标轴指示器配置项。
             type: 'cross', // 指示器类型。当前为十字准星指示器。其实是种简写，表示启用两个正交的轴的 axisPointer。
             link: { xAxisIndex: 'all' }
-        },
-        backgroundColor: 'rgba(245, 245, 245, 0.5)', // 提示框浮层的背景颜色。
-        borderWidth: 1, // 提示框浮层的边框宽。
-        borderColor: '#ccc', // 提示框浮层的边框颜色。
-        padding: 10, // 提示框浮层内边距，单位px
-        textStyle: { // 提示框浮层的文本样式
-            // fontSize: '0.1rem', // 文字的字体大小
-            color: '#ffffff', // 文字的颜色
-            fontFamily: 'Avenir, Helvetica, Arial, sans-serif' // 文字的字体系列
-        },
-        extraCssText: 'background:#252332;border:0;opacity: 1;fontSize: 18px', // 文字本身的描边颜色
+        }
     },
     grid: [ // 直角坐标系内绘图网格
         {
@@ -110,28 +118,39 @@ var macdOption = {
 var macdMobileOption = {
     backgroundColor: '#161b21', // K线图背景色，默认'#161b21'
     animation: false, // 是否开启动画。
+    // tooltip: { // 提示框组件。
+    //     // show: false,
+    //     trigger: 'axis', //触发类型。当前为坐标轴触发
+    //     animation: false,
+    //     axisPointer: { // 坐标轴指示器配置项。
+    //         type: 'cross', // 指示器类型。当前为十字准星指示器。其实是种简写，表示启用两个正交的轴的 axisPointer。
+    //         link: { xAxisIndex: 'all' }
+    //     },
+    //     backgroundColor: 'rgba(245, 245, 245, 0.5)', // 提示框浮层的背景颜色。
+    //     borderWidth: 1, // 提示框浮层的边框宽。
+    //     borderColor: '#ccc', // 提示框浮层的边框颜色。
+    //     padding: 10, // 提示框浮层内边距，单位px
+    //     textStyle: { // 提示框浮层的文本样式
+    //         // fontSize: '0.1rem', // 文字的字体大小
+    //         color: '#ffffff', // 文字的颜色
+    //         fontFamily: 'Avenir, Helvetica, Arial, sans-serif' // 文字的字体系列
+    //     },
+    //     extraCssText: 'background:#252332;border:0;opacity: 1;fontSize: 18px', // 文字本身的描边颜色
+    // },
     tooltip: { // 提示框组件。
         trigger: 'axis', //触发类型。当前为坐标轴触发
         animation: false,
         axisPointer: { // 坐标轴指示器配置项。
             type: 'cross', // 指示器类型。当前为十字准星指示器。其实是种简写，表示启用两个正交的轴的 axisPointer。
             link: { xAxisIndex: 'all' }
-        },
-        backgroundColor: 'rgba(245, 245, 245, 0.5)', // 提示框浮层的背景颜色。
-        borderWidth: 1, // 提示框浮层的边框宽。
-        borderColor: '#ccc', // 提示框浮层的边框颜色。
-        padding: 10, // 提示框浮层内边距，单位px
-        textStyle: { // 提示框浮层的文本样式
-            // fontSize: '0.1rem', // 文字的字体大小
-            color: '#ffffff', // 文字的颜色
-            fontFamily: 'Avenir, Helvetica, Arial, sans-serif' // 文字的字体系列
-        },
-        extraCssText: 'background:#252332;border:0;opacity: 1;fontSize: 18px', // 文字本身的描边颜色
+        }
     },
     grid: [ // 直角坐标系内绘图网格
         {
+            top: 30,
             left: 20,
-            right: 20,
+            right: 10,
+            bottom: 10, // grid 组件离容器下侧的距离。
             // backgroundColor: "#1b2229", // 网格背景色
             borderColor: '#1b2229', // 网格的边框颜色
             show: true // 是否显示直角坐标系网格。
@@ -159,8 +178,10 @@ var macdMobileOption = {
             axisLabel: {
                 show: true,
                 inside: true,
-                fontSize: 22, //
-                color: '#b9cadd', // 刻度标签文字的颜色
+                margin: 0,
+                color: '#9aa4ac',
+                fontSize: 22,
+                verticalAlign: 'bottom'
             }
         }
     ],
