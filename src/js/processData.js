b9ae23a214322739f167dee55f233efe2ff0c67e
zcZhang123 <18856655155@163.com>
2019-03-22 15:13:44 +0800

    添加RSI指标测试

--- src/js/processData.js
+++ src/js/processData.js
@@ -259,7 +259,7 @@ export const getDMIData = (data) => {
 };
 
 export const getTRIXData = (datas) => {
-    if (!datas) { return }
+    if (!datas) { return; }
     var TR = [];
     let TRa = calculateEMAByCandleData(datas, 12);
     let TRb = getEMAData(TRa, 12);
@@ -280,21 +280,21 @@ export const getTRIXData = (datas) => {
 };
 
 export const getRSIData = (datas, periodic) => {
-    if (!datas) { return }
-    let RSI = []
-    let upsAndDowns = []
+    if (!datas) { return; }
+    let RSI = [];
+    let upsAndDowns = [];
     for (let i = 0; i < datas.length; i++) {
         if (i === 0) {
-            upsAndDowns.push(0)
+            upsAndDowns.push(0);
         } else {
-            upsAndDowns.push((datas[i][1] - datas[i - 1][1]) / datas[i - 1][1])
+            upsAndDowns.push((datas[i][1] - datas[i - 1][1]) / datas[i - 1][1]);
         }
     }
     // N日RSI =N日内收盘涨幅的平均值/(N日内收盘涨幅均值+N日内收盘跌幅均值) ×100
 
     for (let i = 0; i < upsAndDowns.length; i++) {
         if (i < periodic - 1) {
-            RSI.push('-')
+            RSI.push('-');
         } else {
             let gains = 0; //涨幅
             let gainsNumber = 0; //涨幅天数 
@@ -302,25 +302,25 @@ export const getRSIData = (datas, periodic) => {
             let dropNumber = 0; //跌幅天数
             for (let j = i - periodic + 1; j < i + 1; j++) {
                 if (upsAndDowns[j] >= 0) {
-                    gains = gains + upsAndDowns[j]
-                    gainsNumber = gainsNumber + 1
+                    gains = gains + upsAndDowns[j];
+                    gainsNumber = gainsNumber + 1;
                 } else {
-                    drop = drop + upsAndDowns[j]
-                    dropNumber = dropNumber + 1
+                    drop = drop + upsAndDowns[j];
+                    dropNumber = dropNumber + 1;
                 }
             }
-            let gainsAverage = gains / gainsNumber
-            let dropAverage = drop / dropNumber
-            let RSIValue = (gainsAverage / (gainsAverage + Math.abs(dropAverage))) * 100
+            let gainsAverage = gains / gainsNumber;
+            let dropAverage = drop / dropNumber;
+            let RSIValue = (gainsAverage / (gainsAverage + Math.abs(dropAverage))) * 100;
             if (isNaN(RSIValue)) {
-                RSI.push(0)
+                RSI.push(0);
             } else {
-                RSI.push(RSIValue)
+                RSI.push(RSIValue);
             }
         }
     }
     return RSI;
-}
+};
 
 export const getMAData = (periodic, data) => {
     var result = [];
--- test/unit/specs/processData.spec.js
+++ test/unit/specs/processData.spec.js
@@ -1,4 +1,4 @@
-import { splitData, getDepthData, handleDivisionData, calculateMA, getOBVData, getDMIData, getTRIXData } from 'js/processData'
+import { splitData, getDepthData, handleDivisionData, calculateMA, getOBVData, getDMIData, getRSIData, getTRIXData } from 'js/processData'
 import testData from '../../testData/data.json'
 import data from '../../testData/testData.json'
 import { getKDJData } from '../../../src/js/processData.js';
@@ -141,6 +141,16 @@ describe('test processData', () => {
   it('test getTRIXData if data is null', () => {
     let TRIXData = getTRIXData(null)
     expect(TRIXData).toBe(undefined)
+  })
+
+  it('test getRSIData', () => {
+    let RSIData = getRSIData(data.candleData.values, 6)
+    expect(RSIData).not.toBeNull()
+  })
+
+  it('test getRSIData if data is null', () => {
+    let RSIData = getRSIData(null)
+    expect(RSIData).toBe(undefined)
   })
 
 })
