8961bfd2aecc358c4e871e5f0a8214bea025798e
zcZhang123 <18856655155@163.com>
2019-06-04 15:11:58 +0800

    添加PSY、ROC、BRAR指标测试

---
+++ test/unit/specs/SetBRARChart.spec.js
@@ -0,0 +1,129 @@
+import SetBRARChart from 'js/SetBRARChart'
+import { getBRARData } from 'js/CalculateIndicator'
+import { StochasticOption } from 'js/IndicatorsLineOption'
+import testData from '../../testData/testData.json'
+
+let BRARData = getBRARData(testData.klineData)
+let indicatorData = {
+    categoryData: testData.candleData.categoryData,
+    indicator: 'BRAR',
+    indicatorData: BRARData
+}
+let size = {
+    height: 1080,
+    width: 1920
+}
+
+describe('test SetBRARChart', () => {
+    it('test SetBRARChart', () => {
+        let brar = new SetBRARChart(StochasticOption);
+        expect(brar).toBeInstanceOf(SetBRARChart)
+    })
+
+    it('test initBRARECharts', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element)
+        brar.initBRARECharts(element, true)
+        expect(brar).not.toBeNull();
+    })
+
+    it('test setBRAROption', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'hour')
+        expect(brar.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test changeDataZoom', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'hour')
+        brar.changeDataZoom('leftShift')
+        expect(brar.indicator.getOption().dataZoom[0].start).toBe(28);
+        expect(brar.indicator.getOption().dataZoom[0].end).toBe(98);
+        brar.changeDataZoom('rightShift')
+        expect(brar.indicator.getOption().dataZoom[0].start).toBe(30);
+        expect(brar.indicator.getOption().dataZoom[0].end).toBe(100);
+        brar.changeDataZoom('enlarge')
+        expect(brar.indicator.getOption().dataZoom[0].start).toBe(35);
+        brar.changeDataZoom('refresh')
+        expect(brar.indicator.getOption().dataZoom[0].start).toBe(30);
+        brar.changeDataZoom('narrow')
+        expect(brar.indicator.getOption().dataZoom[0].start).toBe(25);
+        brar.changeDataZoom('test')
+        expect(brar.indicator.getOption().dataZoom[0].start).toBe(25);
+    })
+
+    it('test setBRAROption if data is null', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(null, 'hour')
+        expect(brar.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test updateBRAROption', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'hour')
+        brar.updateBRAROption(indicatorData, 'week')
+        expect(brar.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test getToolTipData', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'hour')
+        let tipData = brar.getToolTipData()
+        expect(tipData).not.toBeNull();
+    })
+
+    it('test resizeBRARECharts', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'hour')
+        brar.resizeBRARECharts(element, false, size)
+        expect(brar.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test resizeBRARECharts if DOM is null', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'aa')
+        brar.resizeBRARECharts(null, false, size)
+        expect(brar.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test resizeBRARECharts if isFullScreen is true', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, '5minute')
+        brar.resizeBRARECharts(element, true, size)
+        expect(brar.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test disposeBRAREChart', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'hour')
+        brar.disposeBRAREChart()
+        expect(brar.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test updateStochasticOption if getOption is null', () => {
+        const element = document.createElement('div');
+        let brar = new SetBRARChart(StochasticOption);
+        brar.initBRARECharts(element, false, 'init')
+        brar.setBRAROption(indicatorData, 'week')
+        expect(brar).not.toBeNull();
+    })
+})
---
+++ test/unit/specs/SetPSYChart.spec.js
@@ -0,0 +1,129 @@
+import SetPSYChart from 'js/SetPSYChart'
+import { getPSYData } from 'js/CalculateIndicator'
+import { StochasticOption } from 'js/IndicatorsLineOption'
+import testData from '../../testData/testData.json'
+
+let PSYData = getPSYData(testData.klineData)
+let indicatorData = {
+    categoryData: testData.candleData.categoryData,
+    indicator: 'PSY',
+    indicatorData: PSYData
+}
+let size = {
+    height: 1080,
+    width: 1920
+}
+
+describe('test SetPSYChart', () => {
+    it('test SetPSYChart', () => {
+        let psy = new SetPSYChart(StochasticOption);
+        expect(psy).toBeInstanceOf(SetPSYChart)
+    })
+
+    it('test initPSYECharts', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element)
+        psy.initPSYECharts(element, true)
+        expect(psy).not.toBeNull();
+    })
+
+    it('test setPSYOption', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'hour')
+        expect(psy.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test changeDataZoom', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'hour')
+        psy.changeDataZoom('leftShift')
+        expect(psy.indicator.getOption().dataZoom[0].start).toBe(28);
+        expect(psy.indicator.getOption().dataZoom[0].end).toBe(98);
+        psy.changeDataZoom('rightShift')
+        expect(psy.indicator.getOption().dataZoom[0].start).toBe(30);
+        expect(psy.indicator.getOption().dataZoom[0].end).toBe(100);
+        psy.changeDataZoom('enlarge')
+        expect(psy.indicator.getOption().dataZoom[0].start).toBe(35);
+        psy.changeDataZoom('refresh')
+        expect(psy.indicator.getOption().dataZoom[0].start).toBe(30);
+        psy.changeDataZoom('narrow')
+        expect(psy.indicator.getOption().dataZoom[0].start).toBe(25);
+        psy.changeDataZoom('test')
+        expect(psy.indicator.getOption().dataZoom[0].start).toBe(25);
+    })
+
+    it('test setPSYOption if data is null', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(null, 'hour')
+        expect(psy.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test updatePSYOption', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'hour')
+        psy.updatePSYOption(indicatorData, 'week')
+        expect(psy.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test getToolTipData', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'hour')
+        let tipData = psy.getToolTipData()
+        expect(tipData).not.toBeNull();
+    })
+
+    it('test resizePSYECharts', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'hour')
+        psy.resizePSYECharts(element, false, size)
+        expect(psy.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test resizePSYECharts if DOM is null', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'aa')
+        psy.resizePSYECharts(null, false, size)
+        expect(psy.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test resizePSYECharts if isFullScreen is true', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, '5minute')
+        psy.resizePSYECharts(element, true, size)
+        expect(psy.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test disposePSYEChart', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'hour')
+        psy.disposePSYEChart()
+        expect(psy.indicator.getOption()).not.toBeNull();
+    })
+
+    it('test updateStochasticOption if getOption is null', () => {
+        const element = document.createElement('div');
+        let psy = new SetPSYChart(StochasticOption);
+        psy.initPSYECharts(element, false, 'init')
+        psy.setPSYOption(indicatorData, 'week')
+        expect(psy).not.toBeNull();
+    })
+})
