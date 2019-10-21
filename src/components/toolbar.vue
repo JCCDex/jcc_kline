44643fc9a008eea992a48e79170dec76bd366b90
zcZhang123 <18856655155@163.com>
2019-10-21 17:40:14 +0800

    修改PC端页面样式

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
-    <canvas id="toolbar-canvas"  width="1200" height="600"></canvas>
+    <div class="toolbar">
+      <span class="toolbar-iconfont toolbar-span" title="绘制直线" @click="drawLine">&#xe653;</span>
+      <span class="toolbar-iconfont toolbar-span" title="绘制矩形" @click="drawRect">&#xe648;</span>
+      <span class="toolbar-iconfont toolbar-span" title="铅笔" @click="drawTablet">&#xe6be;</span>
+      <span class="toolbar-iconfont toolbar-span" title="橡皮擦" @click="eraser">&#xe6b8;</span>
+      <span class="toolbar-iconfont toolbar-span">
+        <el-color-picker v-model="drawColor" @change="changeDrawColor"></el-color-picker>
+      </span>
+      <span
+        class="toolbar-iconfont toolbar-span"
+        title="线条加粗"
+        @mouseover="showChangeLineWidthSlider"
+        @mouseout="hiddenChangeLineWidthSlider"
+      >
+        &#xe604;
+        <div class="line-width-slider" v-show="showSlider">
+          <el-slider v-model="lineWidth" :min="1" :max="40" @change="changeLineWidth"></el-slider>
+        </div>
+      </span>
+      <span class="toolbar-iconfont toolbar-span" title="撤销" @click="withdraw">&#xe699;</span>
+      <span class="toolbar-iconfont toolbar-span" title="刷新画板" @click="refresh">&#xe747;</span>
+      <span class="toolbar-iconfont toolbar-span" title="保存" @click="save">&#xe69d;</span>
+    </div>
+    <canvas id="toolbar-canvas" width="1200" height="600"></canvas>
   </div>
 </template>
 <script>
